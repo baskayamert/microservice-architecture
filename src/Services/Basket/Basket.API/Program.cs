d11f1bc61b3856371e5128261eca6fda903037b1
Mert Başkaya <m.baskaya@amatis.nl>
2023-02-02 15:12:57 +0300

    Basket API was containerized and Portainer was added

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -27,6 +27,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
+//app.UseCors(builder => builder.WithOrigins("frontend").AllowAnyHeader());
+
 app.MapControllers();
 
 app.Run();
